adb1ec20e05011d4cff769538a0fce6c74296929
Serhii Dychko <dychkosergey@gmail.com>
2022-04-09 16:13:54 +0300

    sync migration with MVP

---
+++ database/migrations/2022_04_09_125924_create_orders_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("status");
+            $table->string("data_name");
+            $table->string("email");
+            $table->string("phone");
+            $table->string("city");
+            $table->string("departament");
+
+            $table->string("promocode_percent");
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')->cascadeOnDelete();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+};
---
+++ database/migrations/2022_04_09_130005_create_liked_product_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('liked_product', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('liked_id');
+            $table->foreign('liked_id')
+                ->references('id')
+                ->on('liked')->cascadeOnDelete();
+
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('liked_product');
+    }
+};
