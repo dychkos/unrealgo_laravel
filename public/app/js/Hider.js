81cd1e14ff5fe2025747e92d918469ab18013761
Serhii Dychko <dychkosergey@gmail.com>
2022-05-02 09:39:02 +0300

    refactored Hider

--- public/app/js/Hider.js
+++ public/app/js/Hider.js
@@ -10,12 +10,10 @@ class Hider {
 
     hide = (event) => {
         let div = $(this.element);
-
         if(this.allowInsideClick){
             this.action();
             return;
         }
-
         if (!div.is(event.target) // если клик был не по нашему блоку
             && div.has(event.target).length === 0) { // и не по его дочерним элементам
             this.action();
