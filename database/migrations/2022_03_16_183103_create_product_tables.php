0ad6e2bce61134f4f2664c9024d2924d385c810c
Serhii Dychko <dychkosergey@gmail.com>
2022-05-14 16:55:14 +0300

    product description

--- database/migrations/2022_03_16_183103_create_product_tables.php
+++ database/migrations/2022_03_16_183103_create_product_tables.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->integer("price");
             $table->integer("offer")->nullable();
 
-            $table->string("description");
+            $table->longText("description");
 
             $table->timestamps();
         });
